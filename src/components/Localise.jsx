fce06cccc79d2a9d54d665a7a157f280087e888a
James Warner <jwarner@amplience.com>
2019-11-13 17:31:46 +0000

    chore(refactor)

--- src/components/Localise.jsx
+++ src/components/Localise.jsx
@@ -1,47 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { translate } from "../hooks/translate";
+import { useTranslation } from "../hooks/translate";
 import { useLocales } from "../hooks/setValue";
-import { defaultValuesFactory } from "../utils/defaultValues";
 
 import { Input } from "./Input";
 import { Button } from "./Button";
 
 export function Localise({ sdk, value }) {
-  const defaultValues = defaultValuesFactory(sdk);
-  const [translated, setTranslated] = useState(defaultValues(value));
-  const [text, setText] = useState([]);
+  const {
+    translated,
+    setText,
+    translateText,
+    getTranslated,
+    updateTranslated,
+    text
+  } = useTranslation(sdk, value);
 
   useLocales(sdk, translated);
-
-  async function translateText() {
-    try {
-      const locales = sdk.locales.available.map(({ language }) => language);
-
-      const { data } = await translate({
-        text,
-        locales
-      });
-
-      const translations = data.reduce((acc, value) => {
-        return Object.assign(acc, { [value.locale]: value.text });
-      }, {});
-
-      setTranslated(translations);
-    } catch (e) {
-      console.error("couldnt translate");
-    }
-  }
-
-  function getTranslated(lang) {
-    return translated[lang];
-  }
-
-  function updateTranslated(lang, value) {
-    const updated = Object.assign({}, translated, { [lang]: value });
-
-    setTranslated(updated);
-  }
 
   return (
     <>
@@ -50,7 +25,7 @@ export function Localise({ sdk, value }) {
         onChange={e => setText(e.target.value)}
         label={"Type text to translate here"}
       />
-      <Button onClick={translateText} label={'Click to translate'}/>
+      <Button onClick={translateText} label={"Click to translate"} />
       {sdk.locales.available.map(({ locale, language }) => (
         <Input
           key={language}
--- src/hooks/translate.js
+++ src/hooks/translate.js
@@ -1,7 +1,9 @@
 import api from '../utils/api';
+import { useState } from 'react';
+import { defaultValuesFactory } from '../utils/defaultValues';
 
 
-export async function translate({ text, locales }) {
+async function translate({ text, locales }) {
   try {
     const translation = await api.post('translate', {text, locales});
 
@@ -9,5 +11,50 @@ export async function translate({ text, locales }) {
   }
   catch(e) {
     throw new Error('failed')
+  }
+}
+
+
+export function useTranslation(sdk, value) {
+  const defaultValues = defaultValuesFactory(sdk);
+  const [translated, setTranslated] = useState(defaultValues(value));
+  const [text, setText] = useState([]);
+  
+  
+  async function translateText() {
+    try {
+      const locales = sdk.locales.available.map(({ language }) => language);
+  
+      const { data } = await translate({
+        text,
+        locales
+      });
+  
+      const translations = data.reduce((acc, value) => {
+        return Object.assign(acc, { [value.locale]: value.text });
+      }, {});
+  
+      setTranslated(translations);
+    } catch (e) {
+      console.error("couldnt translate");
+    }
+  }
+  
+  function getTranslated(lang) {
+    return translated[lang];
+  }
+  
+  function updateTranslated(lang, value) {
+    const updated = Object.assign({}, translated, { [lang]: value });
+  
+    setTranslated(updated);
+  }
+
+  return {
+    text,
+    setText,
+    getTranslated,
+    updateTranslated,
+    translateText
   }
 }
