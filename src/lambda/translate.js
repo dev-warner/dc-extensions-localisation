c1b0e85b877105f9e95a200e0f08692c16f6092a
Karl Lankester-Carthy <klankester@amplience.com>
2019-11-13 15:03:30 +0000

    added translation function to lambda

--- src/lambda/translate.js
+++ src/lambda/translate.js
@@ -1,15 +1,30 @@
 
+import util from 'util'
+import yandex from 'yandex-translate';
+
+const translator = yandex(process.env.KEY);
+const translateText = util.promisify(translator.translate);
+
 async function translate(event, context) {
   try {
     if (event.httpMethod !== 'POST') {
       throw new Error('Unexpected request');
     }
-  
     const body = JSON.parse(event.body);
-  
+
+    const promises = body.locales.map(async (locale) => {
+      const translated = await translateText(body.text, { to: locale });
+
+      if(translated.code !== 200) {
+        return fail(locale)
+      }
+      return success(translated, locale)
+    })
+    const results = await Promise.all(promises)
+
     return {
       statusCode: 200,
-      body: JSON.stringify(body)
+      body: JSON.stringify(results)
     }
   }
   catch (e) {
@@ -21,3 +36,17 @@ async function translate(event, context) {
 }
 
 export const handler = translate;
+
+function fail(locale) {
+  return {
+    text: '',
+    locale
+  };
+}
+
+function success(translated, locale) {
+  return {
+    text: translated.text[0],
+    locale
+  };
+}
